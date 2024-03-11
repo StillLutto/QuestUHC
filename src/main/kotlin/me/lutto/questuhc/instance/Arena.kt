c33a592963b6652a078b29765ce63e34caaf9ce6
StillLutto <iameinsteinhe@gmail.com>
2024-03-11 11:12:11 +0100

    fix: check for max players correctly

--- src/main/kotlin/me/lutto/questuhc/instance/Arena.kt
+++ src/main/kotlin/me/lutto/questuhc/instance/Arena.kt
@@ -73,6 +73,8 @@ class Arena(private val questUHC: QuestUHC, private val id: Int, private val spa
     }
 
     fun addPlayer(player: Player) {
+        if (players.size <= ConfigManager.getMaxRequiredPlayers()) return
+
         players.add(player.uniqueId)
         player.teleport(spawn)
         KitUI(player)
